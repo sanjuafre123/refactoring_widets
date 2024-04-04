f192070c2345791d82193603b58b3de0d36ac42e
Sanju <sanjuafre08@gmail.com>
2024-04-04 07:54:44 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/refactoring_widets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.refactoring_widets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/color/Color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color (0xFF240A34);
+Color grayColor = const Color (0xFF891652);
+Color secondaryColor = const Color (0xFFEABE6C);
+Color bgColor = const Color (0xFFFFEDD8);
+
